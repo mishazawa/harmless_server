36377af851b89f59fc5f2a7906dd836b39e1e9dc
asd <macintoshkrug@gmail.com>
2025-02-21 02:22:46 +0200

    added weather

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import requests
+
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5h://tor:9050',
+                       'https': 'socks5h://tor:9050'}
+    return session
+
+WTTR_CALL = "https://wttr.in/Kyiv?m&format=3"
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/", response_class=PlainTextResponse)
 async def root():
-    return {"message": "Hello World"}
+    session = get_tor_session()
+    res = session.get(WTTR_CALL).text
+    return res
